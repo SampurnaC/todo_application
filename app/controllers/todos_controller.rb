e11f40dba34e5de5e2ada194e0f206a5dfdb052c
SampurnaC <sampurna.chapagainnn@gmail.com>
2020-07-23 22:58:13 +0545

    install bootstrap and list todos and create todos with new form

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,24 @@
 class TodosController < ApplicationController
 
   def index
+    @todos = Todo.all
   end
 
   def new
+    @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    respond_to do |format|
+      if @todo.save
+        format.html {redirect_to root_path}
+      end
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:name, :completed)
+  end
 end
